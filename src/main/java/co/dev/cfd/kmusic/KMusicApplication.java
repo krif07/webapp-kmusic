fb338b7699e393ff3b2bedc1dc9fb932d6b49c54
Cristian Fernando Dávila López <krif07@gmail.com>
2026-05-07 18:37:34 -0500

    Primer commit

---
+++ src/main/java/co/dev/cfd/kmusic/KMusicApplication.java
@@ -0,0 +1,13 @@
+package co.dev.cfd.kmusic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KMusicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KMusicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/dev/cfd/kmusic/KMusicApplicationTests.java
@@ -0,0 +1,13 @@
+package co.dev.cfd.kmusic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KMusicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
