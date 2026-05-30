1144bf9bc809e18d93ce17398d8aef61b5c7657b
Cristian Fernando Dávila López <krif07@gmail.com>
2026-05-29 23:52:53 -0500

    Implement CancionViewController and create listaCanciones.html template for song listing

--- src/main/java/co/dev/cfd/kmusic/controller/CancionViewController.java
+++ src/main/java/co/dev/cfd/kmusic/controller/CancionViewController.java
@@ -3,17 +3,19 @@ package co.dev.cfd.kmusic.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import co.dev.cfd.kmusic.service.CancionService;
 import lombok.AllArgsConstructor;
 
 @Controller
 @AllArgsConstructor
+@RequestMapping(value="/cancion")
 public class CancionViewController {
     
     private final CancionService cancionService;
 
-    @GetMapping(value="canciones")
+    @GetMapping(value="/canciones")
     public String listar(Model model) {
         model.addAttribute("canciones", cancionService.listarCanciones());
         return "listaCanciones";
