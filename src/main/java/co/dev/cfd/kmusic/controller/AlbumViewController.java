5eb478ac7ad445da64cdfe71c930dac4d797c019
Cristian Fernando Dávila López <krif07@gmail.com>
2026-05-30 16:28:26 -0500

    Add AlbumViewController and templates for album management: implement album listing and addition forms

---
+++ src/main/java/co/dev/cfd/kmusic/controller/AlbumViewController.java
@@ -0,0 +1,49 @@
+package co.dev.cfd.kmusic.controller;
+
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import co.dev.cfd.kmusic.model.Album;
+import co.dev.cfd.kmusic.service.AlbumService;
+import co.dev.cfd.kmusic.service.ArtistaService;
+import lombok.AllArgsConstructor;
+
+
+
+@Controller
+@AllArgsConstructor
+@RequestMapping(value="/album")
+public class AlbumViewController {
+ 
+    private final AlbumService albumService;
+    private final ArtistaService artistaService;
+
+    @GetMapping("/albumes")
+    public String listar(Model model) {
+        model.addAttribute("albumes", albumService.obtenerAlbumes());
+
+        return "listarAlbumes";
+    }
+
+    @GetMapping("/agregarAlbum")
+    public String mostrarFormularioNuevoAlbum(Model model) {
+        model.addAttribute("album", new Album());
+        model.addAttribute("artistas", artistaService.listarArtistas());
+
+        return "agregarAlbumForm";
+    }
+
+    @PostMapping("/guardarAlbum")
+    public String postMethodName(@ModelAttribute Album album, 
+            @RequestParam Long idArtista, @RequestParam List<Long> idCanciones) {
+        albumService.guardarAlbum(album, idArtista, idCanciones);
+        return "redirect:/album/albumes";
+    }
+}
